a5c47f66354c7f78a59acd7ed210bc4804fe3ecb
Christophe Chapuis <chris.chapuis@gmail.com>
2020-04-20 18:14:47 +0000

    initramfs-scripts-simple: add recovery mode with mass storage
    This recovery mode is activated when "bootmode=recovery" is found on
    kernel's cmdline.
    In addition to the USB network, it configures also a mass storage gadget,
    which can be used to expose eMMC or SDcard to the host.
    
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- recipes-core/initrdscripts/initramfs-scripts-simple/init.sh
+++ recipes-core/initrdscripts/initramfs-scripts-simple/init.sh
@@ -18,20 +18,30 @@ setup_log
 
 setup_usb_network 172.16.42.2/16
 
-# start telnetd for this IP
-start_telnetd 172.16.42.2
+if grep -q bootmode=recovery /proc/cmdline; then
+    # Add root user
+    cat > /etc/passwd << "EOF"
+root::0:0:root:/root:/bin/sh
+EOF
+    
+    # start telnetd for this IP
+    start_telnetd 172.16.42.2
+    
+    # start... a shell ? a nice UI ?
+    sh
+else
+    # mount partition labeled "luneos-rootfs"
+    mount_root_partition "luneos-rootfs" "/rfs"
 
-# mount partition labeled "luneos-rootfs"
-mount_root_partition "luneos-rootfs" "/rfs"
+    mount_proc_sys_dev_configfs "/rfs"
 
-mount_proc_sys_dev_configfs "/rfs"
+    #info "Stopping debug services"
+    #stop_telnetd
+    stop_mdev
 
-#info "Stopping debug services"
-#stop_telnetd
-stop_mdev
+    info "Umounting unneeded filesystems"
+    umount_proc_sys_dev_configfs ""
 
-info "Umounting unneeded filesystems"
-umount_proc_sys_dev_configfs ""
-
-info "Switching to root filesystem"
-exec switch_root /rfs /sbin/init
+    info "Switching to root filesystem"
+    exec switch_root /rfs /sbin/init
+fi
